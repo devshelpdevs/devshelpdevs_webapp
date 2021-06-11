a841294885febbabc8273f730700636d43b9ebb2
Shahadat Kauser <Shahadatksr@gmail.com>
2021-06-11 09:36:34 +0000

    Removed default styles

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DevsHelpDevs</title>
+        <meta name="description" content="DevsHelpDevs Web App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 >
+          Welcome to <a href="/">DevsHelpDevs!</a>
+        </h1>
+
+      
+      </main>
+    </div>
+  )
+}
