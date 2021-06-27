3edc2cf3c90bd653c5ee63c3b7a07549b2f65c72
Duru-young Raymond <duruyoungcr@gmail.com>
2021-06-27 14:48:51 +0100

    Centered partners section heading

--- components/pages/Partners/index.tsx
+++ components/pages/Partners/index.tsx
@@ -5,7 +5,7 @@ const Partners = () => {
     return (
         <section aria-label='devshelpdevs partners' className='bg-[#F0F0F0] px-8 sm:px-24 py-12 sm:py-32'>
             <div>
-                <h4 className='font-medium text-[#115FA7]'>Partnering hubs</h4>
+                <h4 className='font-medium text-[#115FA7] text-center'>Partnering hubs</h4>
                 <div className='flex justify-between items-center overflow overflow-x-scroll partners'>
                     <div className='mr-8 sm:mr-0'>
                         <Image src='/assets/images/partners/nhost.svg' layout='fixed'  width={'100%'} height={'100%'}/>
