e68dfb7fb38824ddb2dd22590aa793548b3a3f9c
Duru-young Raymond <duruyoungcr@gmail.com>
2021-06-27 14:36:14 +0100

    Added Partners section to the homepage

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -2,10 +2,13 @@
 import Contribute from '../components/pages/contribute/contribute';
 import Testimonials from '../components/pages/home/testimonials';
 import Hero from '../components/pages/home/hero';
+import React from 'react';
+import Partners from '../components/pages/Partners';
 
 const Home = () =>  {
   return <>
         <Hero />
+        <Partners />
         <Contribute/>
         <Testimonials />
 
